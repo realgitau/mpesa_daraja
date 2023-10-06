0db21d02e7e406c27575f59b88842df9e2495223
realgitau <antonygitau04@gmail.com>
2023-10-06 11:07:33 +0300

    created the access token

---
+++ accesstoken.py
@@ -0,0 +1,14 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+# files
+import keys
+
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret 
+api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+headers = { 'Authorization': 'Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' }
+
+r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+print(r.text)
